9a284d5d13b83d4dc9f60d35afe938422ce5681b
tk7622 <tk622@srmist.edu.in>
2026-02-24 16:30:10 +0530

    Printing OOPS Banner using functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,70 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-package OOPSApp;
 
 public class OOPSBannerApp {
-        public static void main(String[] args)
-    {
-        String[] lines = {
-                (String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** ")),
-                (String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      ")),
-                (String.join(" ", "**     **", "**     **", "**     **", "**       ")),
-                (String.join(" ", "**     **", "**     **", "**    ** ", "  *****  ")),
-                (String.join(" ", "**     **", "**     **", "******   ", "      ** ")),
-                (String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** ")),
-                (String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  "))};
-                for(String line: lines){
-                    System.out.println(line);
-                }
+        public static String[] getOPattern() {
+            return new String[] {
+                "    ***   ", 
+                 " **   ** ", 
+                 "**     **", 
+                 "**     **", 
+                 "**     **", 
+                 " **   ** ", 
+                 "   ***   "
+            };
+        }
+            public static String[] getOPattern1() {
+            return new String[] {
+                "   ***   ", 
+                 " **   ** ", 
+                 "**     **", 
+                 "**     **", 
+                 "**     **", 
+                 " **   ** ", 
+                 "   ***   "
+            };
+        }
+            public static String[] getPPattern() {
+            return new String[] {
+                "*********", 
+                "**     **", 
+                "**     **", 
+                "**     **", 
+                "*********", 
+                "**       ", 
+                "**       ", 
+                "**       ", };
+        }
+        public static String[] getSPattern() {
+            return new String[] {
+                " ******  ",
+                "**    ** ",
+                "**       ",
+                "  *****  ",
+                "      ** ",
+                " **   ** ", 
+                "  *****  "};
+        }
+        public static void main(String[] args) {
+            String[] oPattern = getOPattern();
+            String[] oPattern1 = getOPattern();
+            String[] pPattern = getPPattern();
+            String[] sPattern = getSPattern();
 
-    }
-
+            for (String line : oPattern) {
+                System.out.println(line);
+            }
+            System.out.println();
+            for (String line : oPattern1) {
+                System.out.println(line);
+            }
+            System.out.println();
+            for (String line : pPattern) {
+                System.out.println(line);
+            }
+            System.out.println();
+            for (String line : sPattern) {
+                System.out.println(line);
+            }
+        }
 }
 
 
